b01b0d2bdbe5d254953fd5cd9fadca6a8899df65
kiyama-PC\kiyama <kiyam350@gmail.com>
2016-10-08 22:33:10 +0900

    初期ファイル

---
+++ sassサンプル/js/test.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    
+    //ホバーとクリックで開閉
+//    $("#hover").mouseover(function () {
+//        $("#hover-down").slideDown("slow");  
+//    });
+//    
+//    $("#hover").click(function () {
+//        if ($("#hover-down").is(":hidden")) {
+//            $("#hover-down").slideDown("slow");
+//        } else {
+//            $("#hover-down").slideUp("slow");
+//        }    
+//    });
+    
+});
